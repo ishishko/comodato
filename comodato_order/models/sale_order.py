113aa68c61d73fad1c98167c7358c026b6e9c607
Ignacio Shishko <shishkoignacio@gmail.com>
2024-05-31 15:59:13 -0300

    [UPD] Corrigeendo nombre de campo

--- comodato_order/models/sale_order.py
+++ comodato_order/models/sale_order.py
@@ -4,5 +4,5 @@ class SaleOrder (models.Model):
     _inherit='sale.order'
     
 
-    patient_id = fields.many2one('res.partner', string='Paciente', default= False,
+    patient_id = fields.Many2one('res.partner', string='Paciente', default= False,
                                  domain=[('patient_check', '=', True)])
