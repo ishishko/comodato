dd6aaae6574441a67e730c36c58ae2597d48c6ab
Ignacio Shishko <shishkoignacio@gmail.com>
2024-05-31 23:06:08 -0300

    [UPD] Reduciendo Errores v3

--- comodato_order/models/res_partner.py
+++ comodato_order/models/res_partner.py
@@ -3,4 +3,4 @@ from odoo import api, fields, models
 class ResPartnerPatient (models.Model):
     _inherit='res.partner'
     
-    patient = fields.Boolean(string='Paciente', default= False )
+    patient_check = fields.Boolean(string='Paciente', default= False )
