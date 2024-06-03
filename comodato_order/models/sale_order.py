19df12f564e032165efab9b4d772d2ae8bd4deac
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-03 09:21:02 -0300

    [UPD] Agregando campo patient_id a sale_order v4

--- comodato_order/models/sale_order.py
+++ comodato_order/models/sale_order.py
@@ -4,5 +4,6 @@ class SaleOrder (models.Model):
     _inherit='sale.order'
     
 
-    patient_id = fields.Many2one('res.partner', string='Paciente', default= False,
-                                 domain=[('patient_check', '=', True)])
+    # patient_id = fields.Many2one('res.partner', string='Paciente', default= False,
+    #                              domain=[('patient_check', '=', True)])
+    patient_id = fields.Integer(string='Paciente', default= False)
