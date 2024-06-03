5f92e031aa7e658a3159aee39a906157224a078d
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-03 10:41:25 -0300

    [UPD] menu v2

--- comodato_order/__manifest__.py
+++ comodato_order/__manifest__.py
@@ -1,35 +1,32 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Comodato",
-    'summary': "Manejo de contratos de comodato, entrega y devolucion",
-    'description': """
+    "name": "Comodato",
+    "summary": "Manejo de contratos de comodato, entrega y devolucion",
+    "description": """
 Aplicacion para el manejo de comodato de pacientes a traves de empresas.
 Se agrega el campo patient_check a la vista de res.partner.
     """,
-    'autor': 'Ignacio Shishko',
-    'website': "https://www.hitofusion.com/",
-
-    'category': 'Sales/Sales',
-    'sequence': 160,
-    'version': '17.0.0.0.0',
-
-    'depends': ['base','sale'],
-
-    'data': [
-        'views/res_partner_patient.xml',
+    "autor": "Ignacio Shishko",
+    "website": "https://www.hitofusion.com/",
+    "category": "Sales/Sales",
+    "sequence": 160,
+    "version": "17.0.0.0.0",
+    "depends": ["base", "sale"],
+    "data": [
+        "views/res_partner_patient.xml",
+        "views/sale_order_view.xml",
+        "views/sale_comodato_menus.xml",
         # 'views/product_template_view.xml',
         # 'views/product_view.xml',
-        'views/sale_order_view.xml',
-        # 'views/sale_comodato_menus.xml',
         # 'views/sale_rental_menus.xml'
     ],
-    'installable': True,
-    'application': True,
+    "installable": True,
+    "application": True,
     # 'pre_init_hook': '_pre_init_rental',
     # 'assets': {
     #     'web.assets_backend': [
     #         'sale_renting/static/src/**/*',
     #     ],
     # },
-    'license': 'GPL-3',
+    "license": "GPL-3",
 }
