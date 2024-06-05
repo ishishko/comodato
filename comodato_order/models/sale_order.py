11fa4ad5df9bf84a821649743f853a34886cc2e8
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-05 13:03:40 -0300

    [UPD] Habilitando Menu v26

--- comodato_order/models/sale_order.py
+++ comodato_order/models/sale_order.py
@@ -4,8 +4,7 @@ class SaleOrder (models.Model):
     _inherit='sale.order'
     
 
-    # patient_id = fields.Many2one('res.partner', string='Paciente', default= False,
-    #                              domain=[('patient_check', '=', True)])
-    patient_id = fields.Integer(string='Paciente', default= False)
+    patient_id = fields.Many2one('res.partner', string='Paciente', domain=[('patient_check', '=', True)])
+    # patient_id = fields.Integer(string='Paciente', default= False)
 
     comodato_check = fields.Boolean(string='Comodato', default= False )
