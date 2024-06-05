2d17edf83f90be6210fa82b209edb031dbf095df
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-05 06:35:21 -0300

    [UPD] Habilitando Menu v22

--- comodato_order/models/sale_order.py
+++ comodato_order/models/sale_order.py
@@ -3,6 +3,5 @@ from odoo import api, fields, models
 class SaleOrder (models.Model):
     _inherit='sale.order'
     
-    patient_id = fields.Many2one('res.partner', string='Paciente', default= False,
-                                 domain=[('patient_check', '=', True)])
+    patient_id = fields.Many2one('res.partner', string='Paciente', domain=[('patient_check', '=', True)])
     
