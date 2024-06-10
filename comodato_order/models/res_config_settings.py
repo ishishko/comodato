5e6ba304c9eae0623b639aebdf675e16d7378542
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-10 14:44:47 -0300

    [UPD] Habilitando Menu v80

--- comodato_order/models/res_config_settings.py
+++ comodato_order/models/res_config_settings.py
@@ -7,9 +7,25 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
+    # RENTAL company defaults :
 
-    is_comodato = fields.Boolean(string="Documento Predefinido", default=False)
-    # comodato_document = 
+    # Extra Costs
+
+    extra_hour = fields.Float(
+        "Per Hour", related="company_id.extra_hour", readonly=False,
+        help="This is the default extra cost per hour set on newly created products. You can change this value for existing products directly on the product itself.")
+    extra_day = fields.Float(
+        "Per Day", related="company_id.extra_day", readonly=False,
+        help="This is the default extra cost per day set on newly created products. You can change this value for existing products directly on the product itself.")
+    # extra_week = fields.Monetary("Extra Week")
+    min_extra_hour = fields.Integer("Minimum delay time before applying fines.", related="company_id.min_extra_hour", readonly=False)
+    # week uom disabled in rental for the moment
+    extra_product = fields.Many2one(
+        'product.product', string="Delay Product",
+        help="This product will be used to add fines in the Rental Order.", related="company_id.extra_product",
+        readonly=False, domain="[('type', '=', 'service')]")
+
+    module_sale_renting_sign = fields.Boolean(string="Digital Documents")
 
     @api.onchange('extra_hour')
     def _onchange_extra_hour(self):
